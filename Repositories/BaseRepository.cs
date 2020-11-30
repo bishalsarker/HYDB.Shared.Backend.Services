bdf7d8d5d9d9137a4f2221be89a6210c5efe431c
Bishal Sarker <mr.bishalsarker@gmail.com>
2020-11-30 23:34:09 +0600

    connection string dev prod toggle added

--- Repositories/BaseRepository.cs
+++ Repositories/BaseRepository.cs
@@ -7,16 +7,27 @@ namespace HYDB.Services.Repositories
 {
     public class BaseRepository
     {
-        private readonly string _dbConn;
+        private readonly bool _devMode;
+        private readonly string _devDbConn;
+        private readonly string _prodDbConn;
 
         public BaseRepository(IConfiguration config)
         {
-            _dbConn = config.GetSection("DbConfig").GetSection("connString").Value;
+            _devMode = bool.Parse(config.GetSection("DevMode").Value);
+            _devDbConn = config.GetSection("DbConfig").GetSection("Dev").GetSection("connString").Value;
+            _prodDbConn = config.GetSection("DbConfig").GetSection("Prod").GetSection("connString").Value;
         }
 
         public string GetConnectionString()
         {
-            return _dbConn;
+            if(_devMode)
+            {
+                return _devDbConn;
+            }
+            else
+            {
+                return _prodDbConn;
+            }
         }
     }
 }
